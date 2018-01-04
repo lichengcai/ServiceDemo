d101119afebc8bb5af2d4d1df1020dc5a3616d59
lichengcai <lichengcai@koolearn.com>
2018-01-04 14:15:40 +0800

    前台五福

--- app/src/main/java/cc/servicedemo/MainActivity.java
+++ app/src/main/java/cc/servicedemo/MainActivity.java
@@ -1,14 +1,34 @@
 package cc.servicedemo;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button mStart;
     private Button mStop;
+    private Button mBind;
+    private Button mUnbind;
+    private MyService.MyBinder myBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            myBinder = (MyService.MyBinder) service;
+            myBinder.startDownload();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +36,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         mStart = (Button) findViewById(R.id.button_start);
         mStop = (Button) findViewById(R.id.button_stop);
+        mBind = (Button) findViewById(R.id.button_bind);
+        mUnbind = (Button) findViewById(R.id.button_unbind);
         mStop.setOnClickListener(this);
         mStart.setOnClickListener(this);
+        mBind.setOnClickListener(this);
+        mUnbind.setOnClickListener(this);
     }
 
     @Override
@@ -28,8 +52,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startService(startIntent);
                 break;
             case R.id.button_stop:
+                Log.d("MyService", "click Stop Service button");
                 Intent stopIntent = new Intent(MainActivity.this,MyService.class);
                 stopService(stopIntent);
+                break;
+            case R.id.button_bind:
+                Intent bindIntent = new Intent(MainActivity.this,MyService.class);
+                bindService(bindIntent,connection,BIND_AUTO_CREATE);
+                break;
+            case R.id.button_unbind:
+                Log.d("MyService", "click Unbind Service button");
+                unbindService(connection);
                 break;
         }
     }
--- app/src/main/java/cc/servicedemo/MyService.java
+++ app/src/main/java/cc/servicedemo/MyService.java
@@ -1,7 +1,10 @@
 package cc.servicedemo;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -12,10 +15,26 @@ import android.util.Log;
 
 public class MyService extends Service {
     private static final String TAG = "MyService";
+    private MyBinder mBinder = new MyBinder();
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+        Notification notification = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            notification = new Notification.Builder(getApplicationContext())
+                    .setAutoCancel(true)
+                    .setContentTitle("title")
+                    .setContentText("describe")
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setWhen(System.currentTimeMillis())
+                    .build();
+        }
+        startForeground(1, notification);
         Log.d(TAG,"onCreate-----");
     }
 
@@ -34,6 +53,12 @@ public class MyService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
+    }
+
+    class MyBinder extends Binder {
+        public void startDownload() {
+            Log.d(TAG,"startDownload-----");
+        }
     }
 }
